4fae90cdc2716e014c4ef3cd5014aed274e25207
mizdra <pp.mizdra@gmail.com>
2025-03-05 21:28:05 +0900

    create spa page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.css';
+
+const App: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <h1>空白のページ</h1>
+    </div>
+  );
+};
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  test: {},
+  plugins: [react()],
+  css: { modules: { localsConvention: 'camelCase' } },
+  build: { outDir: 'dist' },
 });
