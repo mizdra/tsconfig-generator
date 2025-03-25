bc0b3821476611a132828a9a012b02c93d31a84d
mizdra <pp.mizdra@gmail.com>
2025-03-25 21:41:29 +0900

    add counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './App.module.css';
+
+const Counter: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={incrementCount}>+1</button>
+    </div>
+  );
+};
 
 const App: React.FC = () => {
   return (
     <div className={styles.container}>
-      <h1>空白のページ</h1>
+      <div>
+        <h1>Counter App</h1>
+        <Counter />
+      </div>
     </div>
   );
 };
