aa097794828123d0483bfb48acce352dcc1e3ee3
mizdra <pp.mizdra@gmail.com>
2025-03-25 21:59:54 +0900

    setup css-modules-kit

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
+import cssModulesKit from '@css-modules-kit/eslint-plugin';
+import css from '@eslint/css';
 import mizdra from '@mizdra/eslint-config-mizdra';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  { ignores: ['**/dist', 'src/vite-env.d.ts'] },
+export default defineConfig([
+  globalIgnores(['**/dist', 'src/vite-env.d.ts', 'generated']),
   ...mizdra.baseConfigs,
   ...mizdra.typescriptConfigs,
   ...mizdra.nodeConfigs,
@@ -12,5 +14,10 @@ export default [
       // Write your favorite rules
     },
   },
+  {
+    files: ['**/*.css'],
+    language: 'css/css',
+    extends: [css.configs.recommended, cssModulesKit.configs.recommended],
+  },
   mizdra.prettierConfig,
-];
+]);
