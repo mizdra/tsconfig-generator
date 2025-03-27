1cef06c846c48cfc9b207363e45fc6386e75ddd4
mizdra <pp.mizdra@gmail.com>
2025-03-27 21:03:29 +0900

    create form

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './App.module.css';
+import { getFormProps, getInputProps, useForm } from '@conform-to/react';
 
-const Counter: React.FC = () => {
-  const [count, setCount] = useState(0);
-
-  const incrementCount = () => {
-    setCount(count + 1);
-  };
-
-  return (
-    <div>
-      <h2>Count: {count}</h2>
-      <button onClick={incrementCount}>+1</button>
-    </div>
-  );
-};
+interface Schema {
+  email: string;
+}
 
 const App: React.FC = () => {
+  const [form, fields] = useForm<Schema>({});
+
   return (
     <div className={styles.container}>
       <div>
-        <h1>Counter App</h1>
-        <Counter />
+        <form {...getFormProps(form)}>
+          <label htmlFor={fields.email.id}>Email</label>
+          <input {...getInputProps(fields.email, { type: 'email' })} />
+        </form>
       </div>
     </div>
   );
