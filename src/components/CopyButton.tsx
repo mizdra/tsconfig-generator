1fe910328e4555945776beb6d1f55e3cfc572fa3
mizdra <pp.mizdra@gmail.com>
2025-03-30 00:35:05 +0900

    use popover

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -1,18 +1,31 @@
+import { useRef, useState } from 'react';
+import styles from './CopyButton.module.css';
+
 export function CopyButton({ content }: { content: string }) {
+  const [tooltipText, setTooltipText] = useState('');
+  const tooltipRef = useRef<HTMLDivElement>(null);
   const handleCopy = () => {
     navigator.clipboard
       .writeText(content)
       .then(() => {
-        alert('Copied to clipboard');
+        setTooltipText('Copied!');
+        tooltipRef.current?.showPopover();
       })
       .catch((err) => {
-        alert(`Failed to copy: ${err}`);
+        setTooltipText(`Failed to copy: ${err}`);
+        tooltipRef.current?.showPopover();
       });
   };
 
   return (
-    <button type="button" onClick={handleCopy}>
-      Copy
-    </button>
+    <>
+      <button className={styles.button} type="button" onClick={handleCopy}>
+        Copy
+      </button>
+      {/* MEMO: Not sure if `role=tooltip` is best (ref: https://blog.jxck.io/entries/2024-11-01/tooltip-popover.html#apg) */}
+      <div ref={tooltipRef} role="tooltip" popover="auto" className={styles.tooltip}>
+        {tooltipText}
+      </div>
+    </>
   );
 }
