d7f441b777797b754aff864b4113455cde8f705f
mizdra <pp.mizdra@gmail.com>
2025-03-30 00:49:35 +0900

    add `aria-live="assertive"`

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -23,7 +23,7 @@ export function CopyButton({ content }: { content: string }) {
         Copy
       </button>
       {/* MEMO: Not sure if `role=tooltip` is best (ref: https://blog.jxck.io/entries/2024-11-01/tooltip-popover.html#apg) */}
-      <div ref={tooltipRef} role="tooltip" popover="auto" className={styles.tooltip}>
+      <div ref={tooltipRef} role="tooltip" popover="auto" aria-live="assertive" className={styles.tooltip}>
         {tooltipText}
       </div>
     </>
